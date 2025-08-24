d7368bfda5aec852f19a4ddb310dc818ca2b3d05
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2025-08-24 11:06:35 +0100

    first commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full bg-primary-500 dark:bg-primary-600">
+      <h1 className="text-4xl font-bold text-white">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
