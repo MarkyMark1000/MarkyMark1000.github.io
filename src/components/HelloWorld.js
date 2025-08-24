acbeacafe5c93ab4fabd12a8598dc324778fc59f
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2025-08-24 13:16:07 +0100

    adjust hello world to test github actions

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -4,7 +4,7 @@ const HelloWorld = () => {
   return (
     <div className="flex items-center justify-center min-h-screen w-full bg-primary-500 dark:bg-primary-600">
       <h1 className="text-4xl font-bold text-white">
-        Hello World
+        Hello World New
       </h1>
     </div>
   );
